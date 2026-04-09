03296e6c41f6c78c2981f86e3d373d0b9e56677c
shindekalpesharun <shindekalpesharun@gmail.com>
2026-04-09 21:18:29 +0530

    mod: build api

--- 25buildapi/main.go
+++ 25buildapi/main.go
@@ -1,34 +1,35 @@
 package main
 
-import(
+import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"encoding/json"
+
+	"github.com/gorilla/mux"
 )
 
 // model for course - file
 type Course struct {
-	CourseId string `json:"courseid"`
-	CourseName string `json:"coursename"`
-	CoursePrice int `json:"price"`
-	Author *Author `json:"author"`
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
 }
 
 type Author struct {
 	Fullname string `json:"fullname"`
-	Website string `json:"website"`
+	Website  string `json:"website"`
 }
 
 // fake DB
-var course []course
+var course []Course
 
 // middleware, helper - file
 func (c *Course) IsEmpty() bool {
 	return c.CourseId == "" && c.CourseName == ""
 }
 
-
-func main()  {
+func main() {
 	fmt.Println("hi")
 }
 
@@ -36,16 +37,32 @@ func main()  {
 
 // serve home route
 
-func serveHome(w http.ResponseWriter,r *http.Request){
+func serveHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Welcome to API</h1>"))
 }
 
-func getAllCourses(w http.ResponseWriter,r *http.Request){
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encoder(course)
+	json.NewEncoder(w).Encode(course)
 }
 
-func getOneCourse(w http.ResponseWriter,r *http.Request){
-	
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// params grab id from request
+	params := mux.Vars(r)
+
+	// loop through courses and find matching id
+	for _, course := range course {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	// send response if not found
+	json.NewEncoder(w).Encode(map[string]string{"error": "No course found"})
+
 }
